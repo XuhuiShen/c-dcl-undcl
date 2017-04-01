17064a626af1bb04060e3f8970656cc857e6fdd8
shen <shenxuhui1992@163.com>
2017-04-01 19:04:39 +0800

    first push

---
+++ utility.h
@@ -0,0 +1,20 @@
+#ifndef __UTILITY_H__
+#define __UTILITY_H__
+
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+
+#define MAXTOKEN  100
+#define BUFSIZE 100
+
+void dcl(void);
+void dirdcl(void);
+int gettoken(void);
+extern char datatype[MAXTOKEN]; /* data type = char, int, etc. */
+extern char token[MAXTOKEN];
+extern char out[1000];
+extern char name[MAXTOKEN];
+extern int tokentype;
+
+#endif // __UTILITY_H__
